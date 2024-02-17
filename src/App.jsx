1d7e00bf38d865eb7b06762e6d2f46476bf1295f
Abhiavishek <avi74500@gmail.com>
2024-02-18 00:21:20 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,25 @@ import React, { useState, useEffect } from 'react';
 import './App.css';
 import sound from './assets/music.mp3';
 import 'animate.css';
+import image1 from './assets/img3.jpg'
+import image2 from './assets/img2.jpg'
+import image3 from './assets/img1.jpg'
+import image4 from './assets/img4.jpg'
+import image5 from './assets/img5.jpg'
+import groom from './assets/groom.jpg'
+import image7 from './assets/img7.jpg'
+import image9 from './assets/img9.jpg'
+import image10 from './assets/img10.jpg'
+
+
+
+
 
 function App() {
   const [days, setDays] = useState(0);
   const [hours, setHours] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
-  const [value, setValue] = useState("");
 
   function play() {
     new Audio(sound).play();
@@ -41,9 +53,9 @@ function App() {
  
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center bg-[rgb(69,124,196)] text-white relative overflow-hidden">
+    <div className="min-h-screen flex flex-col items-center justify-center bg-[rgb(23,112,126)] text-white relative overflow-hidden">
       <div className="text-center mb-8 relative z-10 animate__animated animate__fadeIn">
-        <h1 className="text-4xl font-bold mb-4">Engagement Celebration</h1>
+        <h1 className="text-4xl font-bold mb-4 mt-5">Engagement Celebration</h1>
         <p className="text-xl mb-4">Love is not just a verb; it’s you looking at each other!</p>
 
         <button
@@ -55,7 +67,7 @@ function App() {
       </div>
 
       <div className="heart-shape-container">
-  <div className="bg-white p-8 rounded heart-shape shadow-md w-[75vw] text-black relative z-20 animate__animated animate__fadeIn">
+  <div className="bg-[rgb(195,147,39)] p-8 rounded heart-shape shadow-md w-[75vw] text-black relative z-20 animate__animated animate__fadeIn">
     <h2 className="text-4xl font-bold mb-6 text-purple-800">Join Us for the Celebration!</h2>
     <p className="mb-4">
       We are thrilled to invite you to the joyous occasion of the engagement of
@@ -63,12 +75,12 @@ function App() {
 
     <div className="circle-container top-1/2 left-1/2  flex justify-center">
     <img
-      src="https://randomuser.me/api/portraits/women/43.jpg" // Replace with the actual bride's photo URL
+      src={image1} // Replace with the actual bride's photo URL
       alt="Bride's Photo"
       className="h-32 w-32 object-cover rounded-full border-4 border-white shadow-md "
     />
     <img
-      src="https://randomuser.me/api/portraits/men/86.jpg" // Replace with the actual bridegroom's photo URL
+      src={image4} // Replace with the actual bridegroom's photo URL
       alt="Bridegroom's Photo"
       className="h-32 w-32 object-cover rounded-full border-4 border-white shadow-md ml-4"
     />
@@ -76,20 +88,20 @@ function App() {
 
     
     <p className="text-lg font-bold mb-4 text-teal-800">
-      Chinmaya Mishra & Abhishek Mishra
+      Chinmayee Mishra (Chinu) & Abhisek Mishra (Dishu)
     </p>
     <p className="mb-4">
       "Two hearts that beat as one." Let's come together and celebrate this
       beautiful journey of love and commitment.
     </p>
     <p className="mb-4">
-      <span className="font-semibold text-indigo-800">Date:</span> Feb 25, 2024
+      <span className="font-semibold text-indigo-800">Date:-</span> Feb 25, 2024
     </p>
     <p className="mb-4">
-      <span className="font-semibold text-indigo-800">Time:</span> 11:00 AM
+      <span className="font-semibold text-indigo-800">Time:-</span> 11:00 AM
     </p>
-    <p className="mb-4">
-      <span className="font-semibold text-indigo-800">Venue:</span> [Venue Name]
+    <p className="mb-4 text-[rgb(103,212,245)]">
+      <span className="font-semibold text-indigo-800">Venue:-</span> Hotel Sutrupati, Unit 4, Bhubaneswar, Odisha 751001
     </p>
     <p className="mb-4">
       Your presence will make this day even more special, filled with love and
@@ -104,37 +116,112 @@ function App() {
 
       <div className="image-container mb-8 mt-5 animate__animated animate__fadeIn">
         <img
-          src="https://www.theknot.com/tk-media/images/9019bea7-4766-4822-ab2a-3eee5bab64d4"
+          src="https://media.istockphoto.com/id/1434652920/photo/newlyweds-put-wedding-rings-on-their-finger-close-up.jpg?s=612x612&w=0&k=20&c=Ycns-hDBoDQXanehIrOvTrD8OMEnFWzNSvJoNNoGoOI="
           alt=""
           className="h-[80vh] w-[75vw] object-cover rounded-md shadow-lg"
         />
       </div>
 
       <div className="photo-album-container w-[75vw]">
-        <h2 className="text-3xl font-bold mb-4 animate__animated animate__fadeIn">Bride's Photo Album</h2>
-        <div className="flex flex-wrap justify-center gap-4">
-          {[...Array(9)].map((_, index) => (
-            <div key={index} className="max-w-sm bg-white rounded-md overflow-hidden shadow-md animate__animated animate__fadeIn">
+        <h2 className="text-3xl font-bold mb-4 animate__animated animate__fadeIn">Photo Album</h2>
+        <div className="flex flex-wrap justify-center gap-4 mb-7">
+            <div className="max-w-sm bg-white rounded-md overflow-hidden shadow-md animate__animated animate__fadeIn">
               <img
-                src={`https://placekitten.com/30${index}/20${index}`}  // Replace with actual photo URLs
-                alt={`Bride's Photo ${index + 1}`}
-                className="w-full h-48 object-cover"
+                src={image2}  // Replace with actual photo URLs
+                alt={`Bride's Photo`}
+                className="w-full h-[220px] object-cover"
               />
               <div className="p-4">
-                <p className="text-xl font-bold mb-2">Photo {index + 1}</p>
-                <p className='text-[red]'>Beautiful moments with the bride.</p>
+                <p className='text-[red]'>Smiling in Solitude.</p>
               </div>
             </div>
-          ))}
+
+            <div className="max-w-sm bg-white rounded-md overflow-hidden shadow-md animate__animated animate__fadeIn">
+              <img
+                src={image7}  // Replace with actual photo URLs
+                alt={`Bride's Photo`}
+                className="w-full h-[220px] object-cover"
+              />
+              <div className="p-4">
+                <p className='text-[red]'>Blessed Moments at Kedarnath Temple.</p>
+              </div>
+            </div>
+
+            <div className="max-w-sm bg-white rounded-md overflow-hidden shadow-md animate__animated animate__fadeIn">
+              <img
+                src={image3}  // Replace with actual photo URLs
+                alt={`Bride's Photo`}
+                className="w-full h-[220px] object-cover"
+              />
+              <div className="p-4">
+                <p className='text-[red]'>A Snapshot of Pure Happiness.</p>
+              </div>
+            </div>
+
+            <div className="max-w-sm bg-white rounded-md overflow-hidden shadow-md animate__animated animate__fadeIn">
+              <img
+                src={groom}  // Replace with actual photo URLs
+                alt={`Bride's Photo`}
+                className="w-full h-[220px] object-cover"
+              />
+              <div className="p-4">
+                <p className='text-[red]'>Cherished moments side by side.</p>
+              </div>
+            </div>
+
+            <div className="max-w-sm bg-white rounded-md overflow-hidden shadow-md animate__animated animate__fadeIn">
+              <img
+                src={image5}  // Replace with actual photo URLs
+                alt={`Bride's Photo`}
+                className="w-full h-[220px] object-cover"
+              />
+              <div className="p-4">
+                <p className='text-[red]'>Shared smiles, creating beautiful memories.</p>
+              </div>
+            </div>
+
+            <div className="max-w-sm bg-white rounded-md overflow-hidden shadow-md animate__animated animate__fadeIn">
+              <img
+                src={image4}  // Replace with actual photo URLs
+                alt={`Bride's Photo`}
+                className="w-full h-[220px] object-cover"
+              />
+              <div className="p-4">
+                <p className='text-[red]'>A Boy's Moment.</p>
+              </div>
+            </div>
+
+            <div className="max-w-sm bg-white rounded-md overflow-hidden shadow-md animate__animated animate__fadeIn">
+              <img
+                src={image9}  // Replace with actual photo URLs
+                alt={`Bride's Photo`}
+                className="w-full h-[220px] object-cover"
+              />
+              <div className="p-4">
+                <p className='text-[red]'>In the Glowing Night.</p>
+              </div>
+            </div>
+
+            <div className="max-w-sm bg-white rounded-md overflow-hidden shadow-md animate__animated animate__fadeIn">
+              <img
+                src={image10}  // Replace with actual photo URLs
+                alt={`Bride's Photo`}
+                className="w-full h-[220px] object-cover"
+              />
+              <div className="p-4">
+                <p className='text-[red]'>The Groom's Smile.</p>
+              </div>
+            </div>
+
+            
         </div>
       </div>
       <footer className="bg-[rgb(69,124,196)] text-white p-8 mt-auto w-full text-center">
     <div className="text-4xl font-extrabold mb-4">Contact Us</div>
     <div className='text-lg font-medium'>
-        <p className='mb-2'>Email: <a href="mailto:engagement@example.com" className="text-yellow-300 hover:underline">engagement@example.com</a></p>
-        <p className='mb-2'>Phone 1: <a href="tel:+11234567890" className="text-yellow-300 hover:underline">+1 123-456-7890</a></p>
-        <p className='mb-2'>Phone 2: <a href="tel:+11234567890" className="text-yellow-300 hover:underline">+1 123-456-7890</a></p>
-        <p className='mb-2'>Address: 123 Love Lane, Cityville</p>        <p className='mt-4'>&copy; {new Date().getFullYear()} Puja Mishra. All rights reserved.</p>
+        <p className='mb-2'>Phone 1: <a href="tel:+11234567890" className="text-yellow-300 hover:underline">+91 890-498-4380</a></p>
+        <p className='mb-2'>Phone 2: <a href="tel:+11234567890" className="text-yellow-300 hover:underline">+91 981-967-2456</a></p>
+        <p className='mb-2'>Address: <a className="text-yellow-300 hover:underline">Plot No.6, Chhaya Complex, Unit 4 Main St, AG Colony, Unit 4, Bhouma Nagar, Bhubaneswar, Odisha 751001 </a> </p>        <p className='mt-4 text-black'>&copy; {new Date().getFullYear()} Puja Mishra. All rights reserved.</p>
 
     </div>
 </footer>
